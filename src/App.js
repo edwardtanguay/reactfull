6aab57232db2623b2989515bf210049b91e5b1d8
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-05-16 21:47:22 +0200

    base formated site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
+import Navbar from './Navbar';
+import {
+	BrowserRouter as Router,
+	Route
+} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Navbar />
+				<div id="page-body">
+					<Route path="/" component={HomePage} exact />
+					<Route path="/about" component={AboutPage} exact />
+					<Route path="/articles-list" component={ArticlesList} exact />
+					<Route path="/article" component={ArticlePage} exact />
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
