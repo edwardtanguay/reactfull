c7ec389dc9b42a8e92a6f3128b2b9d0585fea32d
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-05-17 13:25:47 +0200

    article links work

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import articleContent from './article-content';
 
-const ArticlePage = () => (
-	<>
-		<h1>This is an Article</h1>
-	</>
-)
+const ArticlePage = ({ match }) => {
+	const name = match.params.name;
+	const article = articleContent.find(m => m.name === name);
+
+	if (!article) return <h1>Article doesn't exist.</h1>;
+
+	return (
+		<>
+			<h1>{article.title}</h1>
+			{article.content.map((text, key) => (
+				<p key={key}>{text}</p>
+			))}
+		</>
+	)
+}
 
 export default ArticlePage;
